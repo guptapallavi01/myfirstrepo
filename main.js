1cf36a0cc6eb4ae5eae241e2bf2f7d0f5a12b579
guptapallavi01 <118976487+guptapallavi01@users.noreply.github.com>
2022-11-30 15:40:32 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 function Calc(value1, value2, value3) {
     console.log('Total Sum:', value1 + value2 + value3);
 }
+Calc(4,8,9);
+
+function Calc(value1, value2, value3) {
+    console.log('Total Sum:', value1 + value2 + value3);
+}
 Calc(2, 5, 6);
