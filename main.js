c6d0db706b081570b7809cdacfa890d9da223de8
Pallavi Gupta <pallavi.gupta@habilelabs.io>
2022-11-30 15:43:40 +0530

    add new code "merge"

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ function Calc(value1, value2, value3) {
     console.log('Total Sum:', value1 + value2 + value3);
 }
 Calc(2, 5, 6);
+
+function Calc(value1, value2, value3) {
+    console.log('Total Sum:', value1 + value2 + value3);
+}
+Calc(10,80,60);
