81182f50008569d48d9d61765f3c215ee2a928f9
Pallavi Gupta <pallavi.gupta@habilelabs.io>
2022-12-01 12:18:55 +0530

    updated commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function Calc(value1, value2, value3) {
     console.log('Total Sum:', value1 + value2 + value3);
 }
-Calc(2, 5, 6);
+Calc(12,15,16);
 
 function Calc(value1, value2, value3) {
     console.log('Total Sum:', value1 + value2 + value3);
